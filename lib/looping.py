454bb70d673f019854a618036f679302227cf10c
Eric Song <ericksong91@gmail.com>
2023-09-23 19:16:57 -0400

    Done with assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    square_ints = [element * element for element in int_list]
+    return square_ints
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if (i % 3 == 0) and (i % 5 != 0):
+            print("Fizz")
+        elif (i % 5 == 0) and (i % 3 != 0):
+            print("Buzz")
+        elif i % 15 == 0:
+            print("FizzBuzz")
+        else:
+            print(i)
